9bccd3839935e921ad3ef98aa28783a939854824
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-08-26 02:04:51 +0300

    add dependencies

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,25 @@
 <template>
-    <pre>{{ meals }}</pre>
+    <div class="flex p-8 flex-col">
+        <input 
+            type="text" 
+            class="rounded border-2 border-gray-200 w-full" 
+            placeholder="Search for Meals"
+        >
+
+        <div class="flex justify-center gap-2 mt-2">
+            <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
+                {{ letter }}
+            </router-link>
+        </div>
+    </div>
 </template>
 
 <script setup>
-import { computed } from 'vue'
-import store from '../store';
+    import { computed } from 'vue'
+    import store from '../store';
 
     const meals = computed(() => store.state.meals)
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+
+
 </script>
