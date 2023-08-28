edcc9342872f825b710028fe50bc6b9af74b6fee
Inácio Raimundo <84747204+inacio000@users.noreply.github.com>
2023-08-28 17:34:09 +0300

    add new routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import MealList from '../views/MealList.vue'
+import SearchByIngredient from '../views/SearchByIngredient.vue'
+import SearchByLetter from '../views/SearchByLetter.vue'
+import SearchByName from '../views/SearchByName.vue'
+import DefaultLayout from '../components/DefaultLayout.vue'
+import GuestLayout from '../components/GuestLayout.vue'
 
 const routes = [
     {
-        path: "/",
-        name: "home",
-        component: Home,
-    },    
-    {
-        path: "/letter/:letter",
-        name: "byLetter",
-        component: MealList,
+        path: '/',
+        component: DefaultLayout,
+        children: [
+
+            {
+                path: "/",
+                name: "home",
+                component: Home,
+            },
+            {
+                path: "/by-letter/:letter?",
+                name: "byLetter",
+                component: SearchByLetter,
+            },
+            {
+                path: "/by-ingredient/:ingredient?",
+                name: "byIngredient",
+                component: SearchByIngredient,
+            },
+
+        ]
     },
+    {
+        path: '/guest',
+        component: GuestLayout,
+        children: [
+            {
+                path: "/by-name/:name?",
+                name: "byName",
+                component: SearchByName,
+            },
+        ]
+    }
 ];
 
 const route = createRouter({
